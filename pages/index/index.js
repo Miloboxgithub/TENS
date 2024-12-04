b6879d56dc0caa5f16362d43986b997176191500
Roxy <1012436741@qq.com>
2024-12-04 22:53:19 +0800

    demo1

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,35 @@
 
 Page({
   data: {
-
+    swiperList: [{
+      id: 0,
+      type: 'image',
+      url: 'https://picsum.photos/700/402'
+    }, {
+      id: 1,
+      type: 'image',
+      url: 'https://picsum.photos/700/301',
+    }, {
+      id: 2,
+      type: 'image',
+      url: 'https://picsum.photos/700/300'
+    }, {
+      id: 3,
+      type: 'image',
+      url: 'https://picsum.photos/700/302'
+    }, {
+      id: 4,
+      type: 'image',
+      url: 'https://picsum.photos/700/303'
+    }, {
+      id: 5,
+      type: 'image',
+      url: 'https://picsum.photos/700/304'
+    }, {
+      id: 6,
+      type: 'image',
+      url: 'https://picsum.photos/700/305'
+    }],
     },
 
   bindViewTap() {
--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -7,5 +7,68 @@ Page({
   },
   onShow(){
 
-  }
+  },
+  nav(){
+
+  },
+  logins(){
+    wx.showToast({
+      title: '登录成功',
+    },1000)
+    setTimeout(()=>{
+      wx.switchTab({
+        url: '/pages/index/index',
+      })
+    },1000)
+  },
+  getPhoneNumber (e) {
+    wx.showToast({
+      title: '登录成功',
+    },1000)
+    setTimeout(()=>{
+      wx.switchTab({
+        url: '/pages/index/index',
+      })
+    },1000)
+   
+    const { errMsg, encryptedData, iv } = e.detail;
+console.log(errMsg)
+    if (errMsg === 'getPhoneNumber:ok') {
+      // 获取成功，处理 encryptedData 和 iv
+      //this.handlePhoneNumber(encryptedData, iv);
+      console.log(1111)
+      
+    } else {
+      // 获取失败，处理错误
+      // wx.showToast({
+      //   title: '获取手机号码失败',
+      //   icon: 'none'
+      // });
+    }
+
+
+  },
+  handlePhoneNumber(encryptedData, iv){
+    console.log(encryptedData, iv)
+    wx.request({
+      url: `${apiUrl}/wx/login`, // 拼接完整的 URL
+      method: 'POST',
+      data:{
+        js_code:wx.getStorageSync('code')
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: (res) => {
+        if (res.statusCode === 200) {
+          console.log(res,'login')
+        } else {
+          console.error('请求失败:', res);
+        }
+      },
+      fail: (err) => {
+        console.error('请求失败:', err);
+      },
+    });
+  },
 })
